0299fca8072c567011dac9713506ba094fb96a8a
sam <salmankhan91108@gmail.com>
2025-07-30 18:12:37 +0600

    266A problem solved .

---
+++ CodeFoeces/stoneOnTheTable.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class stoneOnTheTable {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        String str = input.next();
+        int count = 0;
+
+        for(int i = 0; i < n-1; i++){
+            if(str.charAt(i) == str.charAt(i+1)){
+                count++;
+            }
+        }
+        System.out.println(count);
+        input.close();
+    }
+}
