81ce897e78503350452452907756729fb5faee5a
sam <salmankhan91108@gmail.com>
2025-08-06 11:57:00 +0600

    using try for more accurate coding .

--- CodeFoeces/BuyAShovel.java
+++ CodeFoeces/BuyAShovel.java
@@ -2,19 +2,19 @@ import java.util.Scanner;
 
 public class BuyAShovel {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int k = input.nextInt();
-        int r = input.nextInt();
-        int n;
-        
-        for (n = 1; ; n++) {
-            int total = n * k;
-            if (total % 10 == 0 || total % 10 == r) {
-                break;
+        try (Scanner input = new Scanner(System.in)) {
+            int k = input.nextInt();
+            int r = input.nextInt();
+            int n;
+            
+            for (n = 1; ; n++) {
+                int total = n * k;
+                if (total % 10 == 0 || total % 10 == r) {
+                    break;
+                }
             }
+            System.out.println(n);
+            input.close();
         }
-        
-        System.out.println(n);
-        input.close();
     }
 }
