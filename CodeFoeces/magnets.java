37e359ff824c217c1bf7c8c58cafb01fbf000bb9
sam <salmankhan91108@gmail.com>
2025-08-22 11:29:54 +0600

    344A - magnets.

--- CodeFoeces/magnets.java
+++ CodeFoeces/magnets.java
@@ -1,12 +1,25 @@
 import java.util.Scanner;
+
 public class magnets {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
         int n = input.nextInt();
-        for(int i = 0; i < n; i++){
-            String currentmagnate = input.nextLine();
-            
+        int group = 1;
+        
+        input.nextLine();
+        
+        String prevMagnet = input.nextLine();
+        
+        for(int i = 1; i < n; i++) {
+            String currentMagnet = input.nextLine();
+
+            if(prevMagnet.charAt(1) == currentMagnet.charAt(0)) {
+                group++;
+            }
+            prevMagnet = currentMagnet;
         }
+        
+        System.out.println(group);
+        input.close();
     }
 }
