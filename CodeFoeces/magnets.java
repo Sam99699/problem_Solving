5834f842860d3db945e22064f620414d5f93bb8c
sam <salmankhan91108@gmail.com>
2025-08-22 11:20:25 +0600

    JasonFile.

---
+++ CodeFoeces/magnets.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class magnets {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        for(int i = 0; i < n; i++){
+            String currentmagnate = input.nextLine();
+            
+        }
+    }
+}
